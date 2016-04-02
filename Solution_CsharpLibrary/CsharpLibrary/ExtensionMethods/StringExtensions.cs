3a3a649114e4a07b03053857c5aa9d65f91e6b06
Dave Cooper <david@developersheffield.co.uk>
2016-04-02 19:20:55 +0100

    Added string extention method ToCsvFriendly()

---
+++ Solution_CsharpLibrary/CsharpLibrary/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace CsharpLibrary.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Sanitises a string so it is suitable for use as a "cell" is a CSV file
+        /// </summary>
+        /// <param name="text">Original string</param>
+        /// <returns>Sanitised string</returns>
+        public static string ToCsvFriendly(this string text)
+        {
+            if (!(text.Contains(",") || text.Contains("\"")))
+                return text;
+
+            // Replace quotes with double quotes to escape quotes and wrap all text in quotes to avoid comma's breaking the text
+            return string.Format("\"{0}\"", text.Replace("\"", "\"\""));
+        }
+    }
+}
---
+++ Solution_CsharpLibrary/CsharpLibrary_Tests/ExtensionMethods/StringExtensions/ToCsvFriendlyTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CsharpLibrary.ExtensionMethods;
+using NUnit.Framework;
+
+namespace CsharpLibrary_Tests.ExtensionMethods.StringExtensions
+{
+    [TestFixture]
+    public class ToCsvFriendlyTests
+    {
+        [Test]
+        public void AlreadyClean()
+        {
+            const string original = "Some text that shouldn't be changed by the ToCsvFriendly method";
+
+            Assert.That(original.ToCsvFriendly().Equals(original, StringComparison.CurrentCulture));
+        }
+
+        [Test]
+        public void ExcapesDoubleQuotes()
+        {
+            const string original = "Some text with \"double quotes\" in it.";
+            const string expectedResult = "\"Some text with \"\"double quotes\"\" in it.\"";
+
+            Assert.That(original.ToCsvFriendly().Equals(expectedResult, StringComparison.CurrentCulture));
+        }
+
+        [Test]
+        public void HandlesComma()
+        {
+            const string original = "This, text, is, comma, separated";
+            const string expectedResult = "\"This, text, is, comma, separated\"";
+
+            Assert.That(original.ToCsvFriendly().Equals(expectedResult, StringComparison.CurrentCulture));
+        }
+
+        [Test]
+        public void HandlesCommasAndQuotes()
+        {
+            const string comma = "This, text, is, comma, separated";
+            const string doubleQuotes = "Some text with \"double quotes\" in it.";
+            const string expectedResult = "\"This, text, is, comma, separated\",\"Some text with \"\"double quotes\"\" in it.\"";
+
+            Assert.That((String.Join(",", comma.ToCsvFriendly(), doubleQuotes.ToCsvFriendly())).Equals(expectedResult, StringComparison.CurrentCulture));
+        }
+    }
+}
