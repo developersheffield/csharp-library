249e05eeecda6cf133fb76ca81c2ab02837f6934
Dave Cooper <david@developersheffield.co.uk>
2016-04-16 17:57:52 +0100

    Added IsListItem extension method for RepeaterItemEventArgs class

---
+++ Solution_CsharpLibrary/CsharpLibrary/ExtensionMethods/RepeaterItemEventArgsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.UI.WebControls;
+
+namespace CsharpLibrary.ExtensionMethods
+{
+    public static class RepeaterItemEventArgsExtensions
+    {
+        public static bool IsListItem(this RepeaterItemEventArgs repeaterItemEventArgs)
+        {
+            return repeaterItemEventArgs.Item.ItemType == ListItemType.Item || repeaterItemEventArgs.Item.ItemType == ListItemType.AlternatingItem;
+        }
+    }
+}
