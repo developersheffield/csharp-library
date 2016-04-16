cfc420c89886d13b434f64cec7fe036004cbe9d6
Dave Cooper <david@developersheffield.co.uk>
2016-04-16 18:24:56 +0100

    Added ToRomanNumerals extension method to int

--- Solution_CsharpLibrary/CsharpLibrary/ExtensionMethods/IntExtensions.cs
+++ Solution_CsharpLibrary/CsharpLibrary/ExtensionMethods/IntExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace CsharpLibrary.ExtensionMethods
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsharpLibrary.ExtensionMethods
 {
     public static class IntExtensions
     {
@@ -10,6 +14,38 @@
         public static bool IsEven(this int integer)
         {
             return integer % 2 == 0;
+        }
+
+        public static string ToRomanNumerals(this int integer)
+        {
+            var result = new StringBuilder();
+            var romanNumeralsMap = new SortedDictionary<int, string>
+                               {
+                                   { 1, "I" },
+                                   { 4, "IV" },
+                                   { 5, "V" },
+                                   { 9, "IX" },
+                                   { 10, "X" },
+                                   { 40, "XL" },
+                                   { 50, "L" },
+                                   { 90, "XC" },
+                                   { 100, "C" },
+                                   { 400, "CD" },
+                                   { 500, "D" },
+                                   { 900, "CM" },
+                                   { 1000, "M" },
+                               };
+
+            foreach (var mapping in romanNumeralsMap.Reverse())
+            {
+                while (integer >= mapping.Key)
+                {
+                    integer -= mapping.Key;
+                    result.Append(mapping.Value);
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
---
+++ Solution_CsharpLibrary/CsharpLibrary_Tests/ExtensionMethods/IntExtensions/ToRomanNumeralsTests.cs
@@ -0,0 +1,20 @@
+﻿using CsharpLibrary.ExtensionMethods;
+using NUnit.Framework;
+
+namespace CsharpLibrary_Tests.ExtensionMethods.IntExtensions
+{
+    [TestFixture]
+    public class ToRomanNumeralsTests
+    {
+        [TestCase(1, ExpectedResult = "I")]
+        [TestCase(64, ExpectedResult = "LXIV")]
+        [TestCase(226, ExpectedResult = "CCXXVI")]
+        [TestCase(900, ExpectedResult = "CM")]
+        [TestCase(998, ExpectedResult = "CMXCVIII")]
+        [TestCase(1712, ExpectedResult = "MDCCXII")]
+        public string ToRomanNumerals(int integer)
+        {
+            return integer.ToRomanNumerals();
+        }
+    }
+}
