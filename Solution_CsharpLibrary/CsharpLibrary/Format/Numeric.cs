0450eeee00f869026340d05b20684cecea078015
Dave Cooper <david@developersheffield.co.uk>
2016-05-14 12:13:03 +0100

    Added number format static strings

---
+++ Solution_CsharpLibrary/CsharpLibrary/Format/Numeric.cs
@@ -0,0 +1,18 @@
+﻿namespace CsharpLibrary.Format
+{
+    /// <summary>
+    /// https://msdn.microsoft.com/en-us/library/dwhawy9k(v=vs.110).aspx
+    /// </summary>
+    public class Number
+    {
+        public static string Currency = "C";
+        public static string Decimal = "D";
+        public static string Exponential = "E";
+        public static string FixedPoint = "F";
+        public static string General = "G";
+        public static string Numeric = "N";
+        public static string Percent = "P";
+        public static string RoundTrip = "R";
+        public static string Hexadecimal = "X";
+    }
+}
