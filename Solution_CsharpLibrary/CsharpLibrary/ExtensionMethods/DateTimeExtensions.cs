08b9f3e2ce41fae7b9ee644e20ac9b29db7189c3
Dave Cooper <david@developersheffield.co.uk>
2016-05-14 14:06:31 +0100

    Added ToJavaScriptMilliseconds extension method to DateTime

---
+++ Solution_CsharpLibrary/CsharpLibrary/ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CsharpLibrary.ExtensionMethods
+{
+    public static class DateTimeExtensions
+    {
+        public static double ToJavaScriptMilliseconds(this DateTime utcDateTime)
+        {
+            return utcDateTime
+                .ToUniversalTime()
+                .Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))
+                .TotalMilliseconds;
+        }
+    }
+}
---
+++ Solution_CsharpLibrary/CsharpLibrary_Tests/ExtensionMethods/DateTimeExtensions/ToJavaScriptMillisecondsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CsharpLibrary.ExtensionMethods;
+using NUnit.Framework;
+
+namespace CsharpLibrary_Tests.ExtensionMethods.DateTimeExtensions
+{
+    [TestFixture]
+    public class ToJavaScriptMillisecondsTests
+    {
+        [Test]
+        public void ToJavaScriptMilliseconds_Past()
+        {
+            var dateTime = new DateTime(1982, 9, 21, 11, 43, 12);
+            Assert.AreEqual(dateTime.ToJavaScriptMilliseconds(), 401452992000);
+        }
+
+        [Test]
+        public void ToJavaScriptMilliseconds_Future()
+        {
+            var dateTime = new DateTime(2020, 1, 31, 16, 55, 1);
+            Assert.AreEqual(dateTime.ToJavaScriptMilliseconds(), 1580489701000);
+        }
+    }
+}
