99618c5fd50d7fdd27cfa95f6b3d43f06cdddc60
Dave Cooper <david@developersheffield.co.uk>
2016-06-12 15:23:28 +0100

    Added secure web app basics

---
+++ Solution_CsharpLibrary/CsharpLibrary/HttpModules/RemoveServerResponseHeader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+
+namespace CsharpLibrary.HttpModules
+{
+    public class RemoveServerResponseHeader : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+        }
+
+        private static void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("Server");
+        }
+
+        public void Dispose() { }
+    }
+}
