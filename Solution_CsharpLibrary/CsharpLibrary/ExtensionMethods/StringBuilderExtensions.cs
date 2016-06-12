a986fdc94d059637f060ee0dfe321f56f578d5d2
Dave Cooper <david@developersheffield.co.uk>
2016-06-12 13:16:48 +0100

    Added StringBuilder extension method AppendLineFormat

---
+++ Solution_CsharpLibrary/CsharpLibrary/ExtensionMethods/StringBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace CsharpLibrary.ExtensionMethods
+{
+    public static class StringBuilderExtensions
+    {
+        public static void AppendLineFormat(this StringBuilder stringBuilder, string format, params object[] args)
+        {
+            stringBuilder.AppendFormat(format, args);
+            stringBuilder.AppendLine();
+        }
+    }
+}
