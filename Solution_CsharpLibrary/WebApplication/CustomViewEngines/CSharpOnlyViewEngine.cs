ba4543c2b2474be84b39359a698b589dac3b1b75
Dave Cooper <david@developersheffield.co.uk>
2016-06-18 17:22:03 +0100

    Added a web application project for front end samples

---
+++ Solution_CsharpLibrary/WebApplication/CustomViewEngines/CSharpOnlyViewEngine.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication.CustomViewEngines
+{
+    public class CSharpOnlyViewEngine : RazorViewEngine
+    {
+        public CSharpOnlyViewEngine()
+        {
+            AreaViewLocationFormats = new[] { "~/Areas/{2}/Views/{1}/{0}.cshtml", "~/Areas/{2}/Views/Shared/{0}.cshtml" };
+            AreaMasterLocationFormats = new [] { "~/Areas/{2}/Views/{1}/{0}.cshtml", "~/Areas/{2}/Views/Shared/{0}.cshtml" };
+            AreaPartialViewLocationFormats = new [] { "~/Areas/{2}/Views/{1}/{0}.cshtml", "~/Areas/{2}/Views/Shared/{0}.cshtml" };
+            ViewLocationFormats = new [] { "~/Views/{1}/{0}.cshtml", "~/Views/Shared/{0}.cshtml" };
+            MasterLocationFormats = new [] { "~/Views/{1}/{0}.cshtml", "~/Views/Shared/{0}.cshtml" };
+            PartialViewLocationFormats = new [] { "~/Views/{1}/{0}.cshtml", "~/Views/Shared/{0}.cshtml" };
+            FileExtensions = new [] { "cshtml" };
+        }
+    }
+}
---
+++ Solution_CsharpLibrary/WebApplication/Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Http;
+using WebApplication.CustomViewEngines;
+
+namespace WebApplication
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // Remove X-AspNetMvc-Version from the response header
+            MvcHandler.DisableMvcResponseHeader = true;
+
+            // Don't add required field attributes to hidden fields
+            DataAnnotationsModelValidatorProvider.AddImplicitRequiredAttributeForValueTypes = false;
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // Optimise ViewEngines
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CSharpOnlyViewEngine());
+        }
+
+        protected void Application_PreSendRequestHeaders()
+        {
+            // Remove response header
+            Response.Headers.Remove("X-AspNetMvc-Version");
+            Response.Headers.Remove("X-AspNet-Version");
+        }
+    }
+}
