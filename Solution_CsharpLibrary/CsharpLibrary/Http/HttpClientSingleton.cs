49eb431aeee3f6b92313f9d18413627f845399ef
Dave Cooper <david@developersheffield.co.uk>
2016-09-04 18:48:03 +0100

    Added a singleton HttpClient

---
+++ Solution_CsharpLibrary/CsharpLibrary/Http/HttpClientSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+
+namespace CsharpLibrary.Http
+{
+    /// <summary>
+    /// Creating and disposing of new HttpClient() can lead to exhausting the sockets available
+    /// Use a single instance of HttpClient for the whole app
+    /// https://github.com/mspnp/performance-optimization/blob/master/ImproperInstantiation/docs/ImproperInstantiation.md
+    /// </summary>
+    public class HttpClientSingleton
+    {
+        private static HttpClient httpClient;
+
+        private HttpClientSingleton() { }
+
+        public static HttpClient HttpClient
+        {
+            get
+            {
+                if (httpClient == null)
+                {
+                    httpClient = new HttpClient();
+                }
+                return httpClient;
+            }
+        }
+    }
+}
---
+++ Solution_CsharpLibrary/CsharpLibrary_Tests/Http/HttpClientSingleton/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CsharpLibrary_Tests.Http.HttpClientSingleton
+{
+    [TestFixture]
+    public class Singleton
+    {
+        [Test]
+        public void Singleton_Created()
+        {
+            var client = CsharpLibrary.Http.HttpClientSingleton.HttpClient;
+            var result = client.GetAsync("http://www.google.co.uk").Result;
+            Assert.IsTrue(result != null && result.IsSuccessStatusCode);
+        }
+    }
+}
