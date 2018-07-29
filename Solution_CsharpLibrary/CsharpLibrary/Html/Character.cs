d8513f9304d0fee11e106afc5ad08006008c5dc0
Dave Cooper <david@developersheffield.co.uk>
2018-07-29 12:20:45 +0100

    HTML characters library.

--- Solution_CsharpLibrary/CsharpLibrary/Html/Character.cs
+++ Solution_CsharpLibrary/CsharpLibrary/Html/Character.cs
@@ -5,10 +5,13 @@
         public const string Ampersand = "&amp;"; // &
 
         public const string Ellipsis = "&hellip;"; // ...
+		public const string Apostrophe = "&apos;"; // '
 
         // https://en.wikipedia.org/wiki/Dash#En_dash_versus_em_dash
         public const string EmDash = "&mdash;"; // - (Break between 2 parts of a sentence)
         public const string EnDash = "&ndash;"; // - (to/range)
+		
+		public const string NonBreakingHyphen = "&#8209;"; // Hyphen that doesn't allow word to break at end of line
 
         public const string Copyright = "&copy;"; // (c)
         public const string Trademark = "&trade;"; // TM
@@ -18,7 +21,7 @@
         public const string FractionThreeQuarters = "&frac34;"; // 3/4
         public const string MathsDivide = "&divide;"; // Maths divide
         public const string MathsMultiple = "&times;"; // Maths multiply
-        public const string Degree = "&deg;"; // e.g. Temperature
+        public const string Degree = "&deg;"; // e.g. Temperature or Angle
 
         public const string LeftDoubleQuote = "&ldquo;"; // " (left)
         public const string RightDoubleQuote = "&rdquo;"; // " (right)
